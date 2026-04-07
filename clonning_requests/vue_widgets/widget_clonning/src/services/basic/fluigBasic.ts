2de4d5a0ef79980a94369d8eb0a874a7b57017d5
CarlosG18 <c.gabriel.abc18@gmail.com>
2026-04-07 17:54:18 -0300

    dando uma enxugada nas classes

--- clonning_requests/vue_widgets/widget_clonning/src/services/basic/fluigBasic.ts
+++ clonning_requests/vue_widgets/widget_clonning/src/services/basic/fluigBasic.ts
@@ -16,25 +16,6 @@ export class FluigService {
     });
   }
 
-  async getRequest(requestId: number) {
-    const url = `/process-management/api/v2/requests/${requestId}`;
-    const response = await this.http.get(url, {
-      params: { expand: "formFields" },
-    });
-    return response.data;
-  }
-
-  async getParameters(solicitationId: string) {
-    const url = "/fluighub/rest/service/execute/datasearch";
-    const payload = {
-      endpoint: "dataset",
-      method: "get",
-      params: `datasetId=dsGetParams&constraintsField=processInstanceId&constraintsInitialValue=${solicitationId}&constraintsFinalValue=${solicitationId}&constraintsType=MUST`,
-    };
-    const response = await this.http.post(url, payload);
-    return response.data;
-  }
-
   async startProcess(payload: any): Promise<number> {
     const encryptedProcessId = await this.crypt(payload.process);
 
@@ -56,18 +37,15 @@ export class FluigService {
         JSON.stringify(body),
       );
 
-      // Se chegar aqui, é sucesso (200 OK)
       const rawMessage = response.data.message;
       const msg =
         typeof rawMessage === "string" ? JSON.parse(rawMessage) : rawMessage;
 
       return msg.processInstanceId;
     } catch (error: any) {
-      // Aqui capturamos o erro 500 e extraímos a mensagem do FluigHub
       if (error.response && error.response.data) {
         const serverMessage = error.response.data.message;
 
-        // Remove tags HTML da mensagem (como o <ul> e <li> do log) para exibir no widget
         const cleanMessage =
           typeof serverMessage === "string"
             ? serverMessage.replace(/<[^>]*>?/gm, "")
